8f880accf711b065f28c858c23a4ef6ada024729
vadapallij <vadapallij@gmail.com>
2025-02-25 15:28:36 -0600

    getting all files into the branch

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // To Display the list of tasks
+    public function index(){
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        Task::create([
+            'title'=> $request->title,
+        ]);
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index']);
+
+route::get('/tasks', [TaskController::class, 'store']);
